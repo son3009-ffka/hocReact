b2cee32a0c934b1ceed4a41950c390beb233b905
Nguyen Minh Son <snguyenminh704@gmail.com>
2025-03-31 21:06:25 +0700

    #28

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // () => {}
+//component =html +css+js
+const MyComponent = () => {
+  return (
+    <div>son dep trai </div>
+  );
+}
+
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction() {
-  //   console.log(">>> run my function")
-  // }
-  const myFunction = () => {
-    console.log(">>> run my arrow function")
-  }
 
-  myFunction();
 
   return (
     <>
@@ -26,6 +27,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello World</h1>
+      <MyComponent></MyComponent>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
