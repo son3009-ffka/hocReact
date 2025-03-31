649508f3e34af5601309ba20622e508f5cbd5e77
Nguyen Minh Son <snguyenminh704@gmail.com>
2025-03-31 22:14:20 +0700

    #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,12 @@
 // () => {}
 //component =html +css+js
+import './style.css';
 const MyComponent = () => {
     return (
-        <div>son dep trai  </div>
+        <>
+            <div className="child" style={{ borderRadius: "10px" }}>son dep trai  </div>
+        </>
+
     );
 }
 export default MyComponent;
